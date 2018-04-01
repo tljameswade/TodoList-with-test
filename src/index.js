241e51a2782873c1bf81252d022ed7ae7d5172d4
Suozhi Qi <sqi@c02vqbk9htd6.wvrgroup.internal>
2018-04-01 15:24:13 -0700

    change some file names

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import Reducer from './reducers/reducer';
+import Reducer from './reducers/index';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 
